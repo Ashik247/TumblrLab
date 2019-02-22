b6bdde49045d68f3e13ad90da17b72bf62fccddd
Ashik Chowdhury <zero@ashiks-macbook-pro.local>
2019-02-21 22:36:43 -0500

    init commit

---
+++ TumblrLab/PhotoCell.swift
@@ -0,0 +1,27 @@
+//
+//  PhotoCell.swift
+//  TumblrLab
+//
+//  Created by Ashik Chowdhury on 2/21/19.
+//  Copyright © 2019 AshikChowdhury. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+
+    
+    @IBOutlet weak var photoView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
